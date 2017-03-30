18ed4218262deecbf53ecfc5f410a66510b5e6bc
mtommarello <mike.tommarello@gmail.com>
2017-03-30 16:52:01 -0400

    Initial Upload
    Initial upload of pages.

---
+++ beers.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+		<!-- jQuery library -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+		<!-- Latest compiled JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>	
+</head>
+<body>
+<?php include 'nav.php'; ?>
+<p> list of beers pulled from database will go here</p>
+</body>
+</html>
---
+++ profile.php
@@ -0,0 +1,3 @@
+<html>
+<p> profile information will go here </p>
+</html>
