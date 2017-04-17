ad66f4cb65c026919e57c2566db5a3ffe1bb9322
srojtas <rojtaszoo@pitt.edu>
2017-04-16 21:28:54 -0400

    Redid Homepage
    Redid the homepage.  Check it out.

---
+++ header.php
@@ -0,0 +1,13 @@
+<head>
+    <meta chartset="utf-8" >
+    <title> Beer Time </title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <link href="https://fonts.googleapis.com/css?family=Didact+Gothic|Domine:400,700" rel="stylesheet">
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
