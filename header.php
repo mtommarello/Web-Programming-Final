8a9532ed5f1303ed850523aa30a064431cdfbe8e
srojtas <rojtaszoo@pitt.edu>
2017-04-16 22:33:34 -0400

    Draft of Homepage Made
    Homepage draft made.  Optimized for mobile as well.

--- header.php
+++ header.php
@@ -4,10 +4,14 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/styles.css">
     <link href="https://fonts.googleapis.com/css?family=Didact+Gothic|Domine:400,700" rel="stylesheet">
+    <link href="css/YTPlayer.css" media="all" rel="stylesheet" type="text/css">
 
     <!-- jQuery library -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 
     <!-- Latest compiled JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    
+    <script src="http://pupunzi.com/mb.components/mb.YTPlayer/demo/inc/jquery.mb.YTPlayer.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
