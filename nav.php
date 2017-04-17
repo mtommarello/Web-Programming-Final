43d2ee6058f52aa4fe399375a2c8ad8d75df0589
srojtas <rojtaszoo@pitt.edu>
2017-04-17 01:02:12 -0400

    Draft of Beers Finished (No Pun Intended)
    I finished a rough draft of the beers page.  The session variable has
    not been added to the page yet.

--- nav.php
+++ nav.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-default">
+<nav class="navbar navbar-default navbar-fixed-top">
     <div class="container-fluid">
         <!-- Brand and toggle get grouped for better mobile display --> <div class="navbar-header">
         <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false"> <span class="sr-only">Toggle navigation</span>
@@ -6,8 +6,8 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="#"></a>
-        <a class="navbar-brand" href="#">Beer Time</a>
+        <a class="navbar-brand" href="index.php"></a>
+        <a class="navbar-brand" href="index.php">Beer Time</a>
         </div>
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
