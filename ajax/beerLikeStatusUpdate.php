308bc05f2151bffeccb51ec5396a90004b04927e
srojtas <sascha.rojtas@pitt.edu>
2017-04-21 21:21:33 -0400

    Updated the Like Status Update Page

--- ajax/beerLikeStatusUpdate.php
+++ ajax/beerLikeStatusUpdate.php
@@ -1,12 +1,12 @@
 <?php
-session_start();    
-include 'dbConnection.php';    
-    $insert = "INSERT INTO ratings (beerID_fk, finalUsersID_fk, rating)
-            VALUES($beerCount, " . $_SESSION['userID']", " . $_POST["likeStatus"] . ");";
+    session_start();    
+    include '../dbConnection.php';
+    $userID = $_SESSION['userID'];
+    $likeStatus = $_POST['likeStatus'];
+    $beerID = $_POST['beerID'];
+    $insert = "INSERT INTO ratings (beerID_fk, finalUsersID_fk, rating) VALUES(" . $beerID . ", " . $userID . ", " . $likeStatus . ");";
     
-     $update = " UPDATE ratings
-            SET rating = " . $_POST["likeStatus"] . ", 
-            WHERE beerID_fk = $beerCount, finalUsersID_fk = " . $_SESSION['userID']";";
+     $update = " UPDATE ratings SET rating = " . $likeStatus . " WHERE beerID_fk =" . $beerID. ", finalUsersID_fk = " . $userID . ";";
     
     if(mysqli_query($dbConnection, $insert)) {
         mysqli_close($dbConnection);
