f6ff35bc9d47d797fcc104395567d9737885f049
mtommarello <mike.tommarello@gmail.com>
2017-04-25 16:36:46 -0400

    Fixed Session on Beer Reviews

--- ajax/beerViewReviews.php
+++ ajax/beerViewReviews.php
@@ -3,37 +3,56 @@
     include '../dbConnection.php';
     $beerName = $_POST['beerName'];
     $query = 'SELECT * FROM reviews INNER JOIN finalUsers ON finalUserID_fk = finalUserID INNER JOIN beers ON beerID_fk = beerID WHERE beerName = "' . $beerName . '";';
-    $userID = $_SESSION["userID"];
-    $queryIfReviewExists = 'SELECT COUNT(*) FROM reviews INNER JOIN beers ON beerID_fk = beerID WHERE beerName = "' . $beerName . '" AND finalUserID_fk = ' . $userID . ';';
-    $countOfReviewsByUser = 0;
+    if($_SESSION) {
+        $userID = $_SESSION["userID"];
+        $queryIfReviewExists = 'SELECT COUNT(*) FROM reviews INNER JOIN beers ON beerID_fk = beerID WHERE beerName = "' . $beerName . '" AND finalUserID_fk = ' . $userID . ';';
+        $countOfReviewsByUser = 0;
 
-    if($resultIfReviewExists = mysqli_query($dbConnection, $queryIfReviewExists)){
-        while ($row = $resultIfReviewExists->fetch_assoc()) {
-            $countOfReviewsByUser = $row["COUNT(*)"];
+        if($resultIfReviewExists = mysqli_query($dbConnection, $queryIfReviewExists)){
+            while ($row = $resultIfReviewExists->fetch_assoc()) {
+                $countOfReviewsByUser = $row["COUNT(*)"];
+            }
         }
-    }
 
-    if ($countOfReviewsByUser == 1) {
-        echo
-            '<script>
-                $(".beerEditDelReviewButtons").show();
-            </script>';
-    } else {
-        echo
-            '<script>
-                $(".beerWriteReviewButton").show();
-            </script>';
-    }
+        if ($countOfReviewsByUser == 1) {
+            echo
+                '<script>
+                    $(".beerEditDelReviewButtons").show();
+                </script>';
+        } else {
+            echo
+                '<script>
+                    $(".beerWriteReviewButton").show();
+                </script>';
+        }
 
-    if ($result = mysqli_query($dbConnection, $query)){
-        echo '<div class="viewReviewsSection">';
-        while ($row = $result->fetch_assoc()) {
-            echo '<div class="reviewFor' . $row["beerName"] . '">';
-                echo '<h3>' . $row["fName"] . ' wrote:</h3>';
-                    echo '<p>' . $row["review"] . '</p>';
+        if ($result = mysqli_query($dbConnection, $query)){
+            echo '<div class="viewReviewsSection">';
+            while ($row = $result->fetch_assoc()) {
+                echo '<div class="reviewFor' . $row["beerName"] . '">';
+                    echo '<h3>' . $row["fName"] . ' wrote:</h3>';
+                        echo '<p>' . $row["review"] . '</p>';
+                echo '</div>';
+            }
             echo '</div>';
         }
-        echo '</div>';
-    }
+    } else {
+            echo
+                '<script>
+                    $(".beerWriteReviewButton").show();
+                </script>';
+        }
+
+        if ($result = mysqli_query($dbConnection, $query)){
+            echo '<div class="viewReviewsSection">';
+            while ($row = $result->fetch_assoc()) {
+                echo '<div class="reviewFor' . $row["beerName"] . '">';
+                    echo '<h3>' . $row["fName"] . ' wrote:</h3>';
+                        echo '<p>' . $row["review"] . '</p>';
+                echo '</div>';
+            }
+            echo '</div>';
+        }
+
 ?>
 
